c31f8f4da44109d60f4779379388ec64b0a0efc5
ZaiPP <paul-zai@mail.ru>
2024-11-14 15:22:13 +0300

    Initial Commit

--- app/src/main/java/ru/hse/speakeasy/app/MainActivity.kt
+++ app/src/main/java/ru/hse/speakeasy/app/MainActivity.kt
@@ -6,10 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.primarySurface
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import ru.hse.speakeasy.app.ui.theme.SpeakEasyTheme
 
@@ -19,13 +26,86 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SpeakEasyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        BottomNavigationBar()
+                    }
+                ) { innerPadding ->
+
                 }
             }
         }
     }
+
+    @Composable
+    fun BottomNavigationBar() {
+        BottomNavigation(
+            backgroundColor = MaterialTheme.colors.primarySurface,
+            contentColor = MaterialTheme.colors.onPrimary
+        ) {
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        ImageVector.vectorResource(id = R.drawable.ic_chat),
+                        contentDescription = "Chat"
+                    )
+                },
+                selected = false,
+                onClick = {
+
+                }
+            )
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        ImageVector.vectorResource(id = R.drawable.ic_camera),
+                        contentDescription = "Camera"
+                    )
+                },
+                selected = false,
+                onClick = {
+
+                }
+            )
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        ImageVector.vectorResource(id = R.drawable.ic_translate),
+                        contentDescription = "Translate"
+                    )
+                },
+                selected = false,
+                onClick = {
+
+                }
+            )
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        ImageVector.vectorResource(id = R.drawable.ic_history),
+                        contentDescription = "History"
+                    )
+                },
+                selected = false,
+                onClick = {
+
+                }
+            )
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        ImageVector.vectorResource(id = R.drawable.ic_fav),
+                        contentDescription = "Favorite"
+                    )
+                },
+                selected = false,
+                onClick = {
+
+                }
+            )
+        }
+    }
 }
+
+
